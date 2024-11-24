b0ee07d3cd3794f907a2dc1585f1eded65a0d5a3
Alexander Aryanfar <alexander.aryanfar@gmail.com>
2024-11-23 20:13:46 -0500

    3commit

--- middleware/generateCardNumber.js
+++ middleware/generateCardNumber.js
@@ -1,32 +1,23 @@
-// Importing required modules
-const express = require('express'); // Express framework for building web apps
-const passport = require('passport'); // Passport.js for user authentication
-const router = express.Router(); // Router instance for defining routes
-const User = require('../models/User'); // Importing the User model
-const bcrypt = require('bcrypt'); // bcrypt for hashing passwords securely
 
-/**
- * Generates a unique 12-digit card number for users.
- * Ensures that the generated card number does not already exist in the database.
- * 
- * @returns {Promise<number>} A unique 12-digit card number
- */
+
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+
+
 const uniqueCardNumber = async () => {
-    let cardNumber; // Variable to store the generated card number
-    let cardExists = true; // Flag to check if the card number already exists in the database
+    let cardNumber;
+    let cardExists = true;
 
-    // Loop until a unique card number is generated
     while (cardExists) {
-        // Generate a random 12-digit card number
         cardNumber = Math.floor(100000000000 + Math.random() * 900000000000);
-
-        // Check if the generated card number exists in the database
         cardExists = await User.findOne({ cardNumber });
     }
 
-    // Return the unique card number
     return cardNumber;
 };
 
-// Export the uniqueCardNumber function as part of an object
-module.exports = { uniqueCardNumber };
+module.exports = { uniqueCardNumber }; // Export as part of an object
